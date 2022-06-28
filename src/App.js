2737a35783218c36c803bc9cb7f5103f7edd0bd0
zcarpen <zcarpen@gmail.com>
2022-06-28 10:07:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Fragment} from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+function App() {
+
+
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import {Fragment} from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpeg';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious foods" />
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header;
